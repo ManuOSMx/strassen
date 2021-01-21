a9d6b6cadad5305d8fdf18e3a8a8c92ae2d2ee55
Mayer Abraham Pérez González <38333546+MayerPerez@users.noreply.github.com>
2021-01-20 21:32:08 -0600

    menos codigo

--- js/matriz_dinamica.js
+++ js/matriz_dinamica.js
@@ -367,126 +367,36 @@ var Matrix = {};
     var colucnas_M1=parseInt(document.getElementById("fil1").value);
     var filas_M2=parseInt(document.getElementById("fil1").value);
     var colucnas_M2=parseInt(document.getElementById("fil1").value);
-    //-------------------------------------------------------------------
     //Creacion de las matrices
     var A = Matrix.new(filas_M1,colucnas_M1,"A"); 
     var B = Matrix.new(filas_M2,colucnas_M2,"B");
     var C;
-    //-------------------------------------------------------------------*/
-    //sacamos valores
-    var contador_filas=0;
-    var contador_cols=0;
-    var c=0;
-    //var a = parseInt(document.matriz1.elements[0].value);
+    
     var conta=0;
-    var numero_de_cosos=0;
-    var numero_de_cosos2=0;
-    //calculamos numero de elementos de la matriz1 dibujada resultado
-    for(var i=0;i<filas_M1;i++){
-      for(var q=0;q<colucnas_M1;q++){
-        numero_de_cosos++;
-      }
-    }
-    //calculamos numero de elementos de la matriz2 dibujada resultado
-    for(var i=0;i<filas_M2;i++){
-      for(var q=0;q<colucnas_M2;q++){
-        numero_de_cosos2++;
-      }
-    }
-    //creamos un arreglo para la matriz resultado
-    var arreglo_resultados = [numero_de_cosos];
-    //convertimos a arreglos las matrices creadas para poderlas manipular
-    //principio matriz 1 matrizar
-    var matrizar1=new Array(2);
-    //INICIALIZAR MATRIZAR 1 DE 2 DIMENSIONES de n elementos
-    for(var n = 0; n<numero_de_cosos;n++){
-      matrizar1[n] = new Array(2);
-    }
-    //Fin de INICIALIZAR MATRIZAR 1 DE 2 DIMENSIONES de n elementos
+    
+    //Se rellena matriz A
     var cosos_de_matrizar1=0;
     for(var k = 0; k<filas_M1;k++){
-      for(var j= 0;j<colucnas_M1;j++){//se obtienen datos AQUI usar funciones strassen
+      for(var j= 0;j<colucnas_M1;j++){
         var n1=parseInt(document.matriz1.elements[cosos_de_matrizar1].value);
-        //matrizar1[k][j]=n1;
         A.set(k,j,n1);//<-----------------------Valores a matriz A
         cosos_de_matrizar1++;
       }
     }
-
-    /*probar matrizar1
-    for(var i=0; i<filas_M1; i++) {
-      for(var j=0; j<colucnas_M1; j++) {
-        document.write(matrizar1[i][j]);
-      }
-    }*/
-
-    //fin matriz 1 matrizar
-    //principio matriz 2 matrizar
-    var matrizar2=new Array(2);
-    //INICIALIZAR MATRIZAR 1 DE 2 DIMENSIONES de n elementos
-    for(var n = 0; n<numero_de_cosos2;n++){
-      matrizar2[n] = new Array(2);
-    }
-    //Fin de INICIALIZAR MATRIZAR 1 DE 2 DIMENSIONES de n elementos
+    //Se rrellena matriz B
     var cosos_de_matrizar2=0;
     for(var k = 0; k<filas_M2;k++){
       for(var j= 0;j<colucnas_M2;j++){
         var n2=parseInt(document.matriz2.elements[cosos_de_matrizar2].value);
-        //matrizar2[k][j]=n2;
         B.set(k,j,n2);//<-----------------------Valores a matriz B
         cosos_de_matrizar2++;
       }
     }
-    /*probar matrizar 2
-      for(var i=0; i<filas_M2; i++) {
-        for(var j=0; j<colucnas_M2; j++) {
-          document.write(matrizar2[i][j]);
-        }
-      } */
 
-    //fin matriz 2 matrizar
-    //------------------------PARTES INNECESARIAS----------------------
-    //pricipio del calculo matriz resultado
-    var Matriz_result = new Array(2);
-    var cosos_de_resultado=0;
-    for(var k = 0; k<filas_M1;k++){
-      for(var j= 0;j<colucnas_M2;j++){
-        cosos_de_resultado++;
-      }
-    }
-    //INICIALIZAR matriz DE 2 DIMENSIONES de n elementos de resultado
-    for(var n = 0; n<cosos_de_resultado;n++){
-      Matriz_result[n] = new Array(2);
-    }
-    //Fin de INICIALIZAR matriz DE 2 DIMENSIONES de n elementos de resultado
-    //operar
-    //inicializo en con un valor 0 todas las posiciones
-    for(var k = 0; k<filas_M1;k++){
-      for(var j= 0;j<colucnas_M2;j++){
-        Matriz_result[k][j]=0;
-      }
-    }
-
-    //calculo
-
-    for(var i=0; i<filas_M1; i++) {
-      for(var j=0; j<colucnas_M2; j++) {
-        for(var k=0;k<colucnas_M1;k++){    
-          Matriz_result[i][j]=Matriz_result[i][j]+ matrizar1[i][k]*matrizar2[k][j];
-        }
-      }
-    }
-
-
-    //-----------------TERMINA CODIGO INNECESARIO------------------------
-    //final de calculo matriz resultado
+    //Se calcula A*B con algoritmo de Strassen
     C = Matrix.strassenMatrixMul(A,B,filas_M1);
 
-    
-    //var n2=parseInt(document.matriz2.elements[0].value);
-    //  var n1=parseInt(document.matriz1.elements[0].value); pruebas
-    //inicio de insersion de resultados en inputs nuevos
-    arreglo_resultados[0]=+n1*n2;
+    //Se muestra C en la pagina
     for(var i=0;i<filas_M1;i++){
       var coso2 = document.createElement("BR");
       document.getElementById("resultado").appendChild(coso2);
