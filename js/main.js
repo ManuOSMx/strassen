fc5fb9e61bea13394b2e5791b61a9a14a3103fc8
Manu-Mx <manuel4612@hotmail.com>
2021-01-23 18:59:33 -0600

    reinicio

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+
+function obtenerDatos() {
+    var tam = parseFloat(document.getElementById('tamMatriz').value);
+    document.matrizN.tamMatriz.value = tam;
+    window.alert(tam);
+}
+//PRUEBA DE EXPORTACION DE FUNCIONES
+/*const exp =  require("./strassen.js");
+
+function f1(){
+    var M = exp.Matriz.new(2,2,"MatrizA");
+    console.log(M.name);
+    console.log("Saludo");
+};
+
+f1();
+/*
+var tama = 2;
+var A = exp.Matriz.new(tama,tama,"A");
+var B = exp.Matriz.new(tama,tama,"B");
+var C;
+var cont = 1;
+console.log("Matriz A");
+//ingresa datos a matriz A
+for(var i =0 ; i<tama;i++){
+  for(var j =0 ; j<tama;j++){
+    A.set(i,j,cont); 
+    cont++;
+  }
+}
+//verifica datos en matriz A
+for(var i =0 ; i<tama;i++){
+  for(var j =0 ; j<tama;j++){
+    console.log(A.get(i,j));
+  }
+}
+console.log("Matriz B");
+//ingresa datos a matriz B
+for(var i =0 ; i<tama;i++){
+  for(var j =0 ; j<tama;j++){
+    B.set(i,j,cont);
+    cont++;
+  }
+}
+//verifica datos en matriz B
+for(var i =0 ; i<tama;i++){
+  for(var j =0 ; j<tama;j++){
+    console.log(B.get(i,j));
+  }
+}
+//Multiplicacion de matrices por Strassen
+C = exp.Matriz.strassenMatrixMul(A,B,tama);//tengo duda con el ultimo parametro de esta funcion
+console.log("Matriz C");
+for(var i =0 ; i<tama;i++){
+  for(var j =0 ; j<tama;j++){
+    console.log(C.get(i,j));
+  }
+}*/
+var btn = document.getElementById('iniciar'),
+    cajSMa = document.getElementById('cajaSMa'),
+    cajSMb = document.getElementById('cajaSMb'),
+    cajSMc = document.getElementById('cajaSMc'),
+    contador = 0;
+
+    function empezarAnimacion() {
+      if(contador == 0){
+        cajaSMa.classList.add('animarCaja');
+        cajaSMb.classList.add('animarCaja');
+        cajaSMc.classList.add('animarCaja');
+        contador = 1;
+      }
+      else {
+        cajaSMa.classList.remove('animarCaja');
+        cajaSMb.classList.remove('animarCaja');
+        cajaSMc.classList.remove('animarCaja');
+        contador = 0;
+      }
+    }
+
+    btn.addEventListener('click',empezarAnimacion,true);
